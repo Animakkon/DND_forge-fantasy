ace8f177a3c9cbbf7e181984f857357499328282
Animakkon <lada.dunskaya@mail.ru>
2024-06-24 17:21:24 +0300

    first structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import PageTemplate from "./templates/PageTemplate.vue";
+import Header from './components/Header.vue';
+import {onMounted} from "vue";
+import {startLoadingData} from "./services/start.service.ts";
+
+onMounted(() => {
+ startLoadingData();
+})
+
+</script>
+
+<template>
+  <PageTemplate>
+    <template #header>
+      <Header></Header>
+    </template>
+    <template #main>
+      <router-view></router-view>
+    </template>
+    <template #footer></template>
+  </PageTemplate>
+</template>
+
+<style scoped>
+</style>
