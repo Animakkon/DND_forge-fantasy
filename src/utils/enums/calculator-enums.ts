eb8717efcdabdb37a8f32fc916b9079c3e6abcb8
Animakkon <lada.dunskaya@mail.ru>
2024-06-29 12:43:36 +0300

    имена в шапку таблицы

---
+++ src/utils/enums/calculator-enums.ts
@@ -0,0 +1,26 @@
+export enum StateNames {
+    STRENGTH = "strength",
+    AGILITY = "agily",
+    PHYSIQUE = "physique",
+    INTELLIGENCE = "intelligence",
+    WISDOM = "wisdom",
+    CHARISMA = "charisma",
+}
+
+export enum RussianStateNames {
+    STRENGTH = "сила",
+    AGILITY = "ловкость",
+    PHYSIQUE = "телосложение",
+    INTELLIGENCE = "интеллект",
+    WISDOM = "мудрость",
+    CHARISMA = "харизма",
+}
+
+export enum RussianShortStateNames {
+    STRENGTH = "сил",
+    AGILITY = "лов",
+    PHYSIQUE = "тел",
+    INTELLIGENCE = "инт",
+    WISDOM = "муд",
+    CHARISMA = "хар",
+}
