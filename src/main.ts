cceffcc58a14bd25cffafbafa6470088c23e4f24
Animakkon <lada.dunskaya@mail.ru>
2024-07-04 09:34:46 +0300

    что используем в приложении

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import router from './router/router.js'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import DataTable from 'primevue/datatable'
+import InputNumber from 'primevue/inputnumber'
+import Column from 'primevue/column'
+import Select from 'primevue/select'
+import Button from "primevue/button";
+import Message from "primevue/message";
+import AutoComplete from 'primevue/autocomplete';
+import { createPinia } from "pinia";
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+
+app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.component('Image', Image);
+app.component('Select', Select);
+app.component('InputNumber', InputNumber);
+app.component('Button', Button);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('AutoComplete', AutoComplete);
+app.component('Message', Message);
+
+app.mount('#app');
